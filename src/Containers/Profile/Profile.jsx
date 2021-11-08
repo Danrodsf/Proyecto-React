b184610e828b6193d67c7ba522174950e7972cce
Danrodsf <90701015+Danrodsf@users.noreply.github.com>
2021-11-08 21:08:09 +0100

    Añadido if en profile para que no envie error al no haber ningun usuario logeado

--- src/Containers/Profile/Profile.jsx
+++ src/Containers/Profile/Profile.jsx
@@ -12,15 +12,31 @@ const Profile = () => {
 
     }, [])
 
-    return (
-        <div className="profileView">
-            <div className="profiles">
-                <div><p>Name:</p>{profileData.name}</div>
-                <div><p>Email:</p>{profileData.email}</div>
-                <div><p>City:</p>{profileData.city}</div>
+    if (profileData) {
+
+        return (
+            <div className="profileView">
+                <div className="container">
+                    <div className="profiles">
+                        <div><p>Name:</p>{profileData.name}</div>
+                        <div><p>Email:</p>{profileData.email}</div>
+                        <div><p>City:</p>{profileData.city}</div>
+                    </div>
+                </div>
             </div>
-        </div>
-    )
+        )
+
+    } else {
+        return (
+            <div className="profileView">
+                <div className="container">
+                    You are not logged in
+                </div>
+            </div>
+        )
+    }
+
+
 
 };
 
