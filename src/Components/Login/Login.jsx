329f097c51144687c786ccb42ace3bc164a4cee4
Danrodsf <Danielrodriguezserafin@gmail.com>
2021-11-16 11:30:51 +0100

    Añadido preloader en Home

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -6,105 +6,112 @@ import { SETSTATE, INITSTATE, LOGIN, LOGOUT } from "../../redux/types";
 import Button from "../Button/Button";
 
 const Login = (props) => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  //Hooks
+  const [msgError, setmsgError] = useState("");
+  const [creds, setCreds] = useState({ email: "", password: "" });
 
-    //Hooks
-    const [msgError, setmsgError] = useState("");
-    const [creds, setCreds] = useState({ email: "", password: "" });
+  //Handlers
+  const inputHandler = (e) => {
+    setCreds({ ...creds, [e.target.name]: e.target.value });
+  };
 
-    //Handlers
-    const inputHandler = (e) => {
-        setCreds({ ...creds, [e.target.name]: e.target.value });
+  const loginBtn = () => {
+    let data = {
+      change: 0,
     };
 
-    const loginBtn = () => {
+    props.dispatch({ type: SETSTATE, payload: data });
+  };
 
-        let data = {
-            change: 0
-        };
-
-        props.dispatch({ type: SETSTATE, payload: data });
-
+  const logIn = async () => {
+    let data = {
+      change: 3,
     };
 
-    const logIn = async () => {
+    props.dispatch({ type: SETSTATE, payload: data });
 
-        let body = {
-            email: creds.email,
-            password: creds.password,
-        };
-
-        try {
-            let res = await axios.post(
-                "https://drs-proyecto-api.herokuapp.com/users/signin",
-                body
-            );
-            let datos = res.data;
-            props.dispatch({ type: LOGIN, payload: datos });
-
-        } catch (error) {
-            setmsgError("Cannot Log In");
-            return;
-        }
+    let body = {
+      email: creds.email,
+      password: creds.password,
     };
 
-    const logOut = () => {
-        props.dispatch({ type: LOGOUT });
-        props.dispatch({ type: INITSTATE })
-        setmsgError('')
-        navigate('/');
-    };
+    try {
+      let res = await axios.post(
+        "https://drs-proyecto-api.herokuapp.com/users/signin",
+        body
+      );
 
-    if (props.state?.change !== "") {
-        if (props.credentials?.token !== "") {
-            return (
-                <div className="loginView">
-                    Logged in as {props.credentials?.user?.name}
-                    <div className="btn" onClick={() => logOut()}>
-                        LOG OUT
-                    </div>
-                    <Button view="PROFILE" url="/profile" />
-                </div>
-            );
-        } else {
-            return (
-                <div className="loginView">
-                    <input
-                        type="email"
-                        name="email"
-                        title="email"
-                        onChange={inputHandler}
-                        lenght="30"
-                        placeholder="email"
-                    />
-                    <input
-                        type="password"
-                        name="password"
-                        title="password"
-                        onChange={inputHandler}
-                        lenght="30"
-                        placeholder="Password"
-                    />
-                    <div className="btn" onClick={() => logIn()}>
-                        LOG IN
-                    </div>
-                    <div className="error">
-                        <h3>{msgError}</h3>
-                    </div>
-                </div>
-            );
-        }
-    } else {
-        return (
-            <div className="btn" onClick={() => loginBtn()}>
-                LOG IN
-            </div>
-        );
+      let datos = res.data;
+      props.dispatch({ type: LOGIN, payload: datos });
+
+      let data = {
+        change: 0,
+      };
+
+      props.dispatch({ type: SETSTATE, payload: data });
+    } catch (error) {
+      setmsgError("Cannot Log In");
     }
+  };
+
+  const logOut = () => {
+    props.dispatch({ type: LOGOUT });
+    props.dispatch({ type: INITSTATE });
+    setmsgError("");
+    navigate("/");
+  };
+
+  if (props.state?.change !== "") {
+    if (props.credentials?.token !== "") {
+      return (
+        <div className="loginView">
+          Logged in as {props.credentials?.user?.name}
+          <div className="btn" onClick={() => logOut()}>
+            LOG OUT
+          </div>
+          <Button view="PROFILE" url="/profile" />
+        </div>
+      );
+    } else {
+      return (
+        <div className="loginView">
+          <input
+            type="email"
+            name="email"
+            title="email"
+            onChange={inputHandler}
+            lenght="30"
+            placeholder="email"
+          />
+          <input
+            type="password"
+            name="password"
+            title="password"
+            onChange={inputHandler}
+            lenght="30"
+            placeholder="Password"
+          />
+          <div className="btn" onClick={() => logIn()}>
+            LOG IN
+          </div>
+          <div className="error">
+            <h3>{msgError}</h3>
+          </div>
+        </div>
+      );
+    }
+  } else {
+    return (
+      <div className="btn" onClick={() => loginBtn()}>
+        LOG IN
+      </div>
+    );
+  }
 };
 
 export default connect((state) => ({
-    credentials: state.credentials,
-    state: state.state
+  credentials: state.credentials,
+  state: state.state,
 }))(Login);
--- src/Containers/Home/Home.jsx
+++ src/Containers/Home/Home.jsx
@@ -1,13 +1,66 @@
-const Home = () => {
+import { useState } from 'react';
+import { connect } from 'react-redux'
 
-    return (
-        <div className="view">
-            <div className="container">
-                <h1>Welcome to MovieAPP</h1>
+const Home = (props) => {
+
+    if (props.state.change < 1) {
+
+        return (
+            <div className="view">
+                <div className="container">
+                    <h1>Welcome to MovieAPP</h1>
+                </div>
             </div>
-        </div>
-    )
+        )
+
+    } else { 
+
+        return (
+            <div className="view">
+                <div className="container">
+                    <div class="dank-ass-loader">
+                        <div class="row">
+                            <div class="arrow up outer outer-18"></div>
+                            <div class="arrow down outer outer-17"></div>
+                            <div class="arrow up outer outer-16"></div>
+                            <div class="arrow down outer outer-15"></div>
+                            <div class="arrow up outer outer-14"></div>
+                        </div>
+                        <div class="row">
+                            <div class="arrow up outer outer-1"></div>
+                            <div class="arrow down outer outer-2"></div>
+                            <div class="arrow up inner inner-6"></div>
+                            <div class="arrow down inner inner-5"></div>
+                            <div class="arrow up inner inner-4"></div>
+                            <div class="arrow down outer outer-13"></div>
+                            <div class="arrow up outer outer-12"></div>
+                        </div>
+                        <div class="row">
+                            <div class="arrow down outer outer-3"></div>
+                            <div class="arrow up outer outer-4"></div>
+                            <div class="arrow down inner inner-1"></div>
+                            <div class="arrow up inner inner-2"></div>
+                            <div class="arrow down inner inner-3"></div>
+                            <div class="arrow up outer outer-11"></div>
+                            <div class="arrow down outer outer-10"></div>
+                        </div>
+                        <div class="row">
+                            <div class="arrow down outer outer-5"></div>
+                            <div class="arrow up outer outer-6"></div>
+                            <div class="arrow down outer outer-7"></div>
+                            <div class="arrow up outer outer-8"></div>
+                            <div class="arrow down outer outer-9"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+
+    }
 
 };
 
-export default Home;
+export default connect((state) => ({
+    credentials: state.credentials,
+    state: state.state
+}))(Home);
